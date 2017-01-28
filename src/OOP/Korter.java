6997f2222a9142309f6274e9ecdfa0b1be4c0973
Spihelga <Sander.pihelgas@itcollege.ee>
2017-01-28 20:48:11 +0200

    Kolmas

--- src/OOP/Korter.java
+++ src/OOP/Korter.java
@@ -4,22 +4,34 @@ package OOP;
  * Created by Sander on 28.01.2017.
  */
 public class Korter {
+
+    int kylalised;
+    int max;
+
     public Korter(int mahutab) {
+        max = mahutab;
 
     }
 
     public void saabus(String taavi) {
-
+        if(kylalised<max) {
+            kylalised++;
+        }
     }
 
     public void prindiKylalisteArv() {
+        System.out.println("Korteris on " + kylalised + " külalist");
 
     }
 
     public void prindiPaljuVeelMahub() {
+        if (kylalised<=max){
+            System.out.println("Korterisse mahub veel " + (max-kylalised) + " inimest");
+        }
+        else System.out.println("Korter on juba täis");
     }
 
     public void lahkus(String taavi) {
-
+        kylalised--;
     }
 }
--- src/OOP/Zelda.java
+++ src/OOP/Zelda.java
@@ -4,17 +4,29 @@ package OOP;
  * Created by Sander on 28.01.2017.
  */
 public class Zelda {
+
+    int zelusid;
+
     public Zelda(int elusid) {
-        
+        zelusid = elusid;
+        //System.out.println(zelusid);
     }
 
     public void kaklusKolliga(int kollilElusid) {
+        zelusid = zelusid - kollilElusid;
+
     }
 
     public void prindiMituEluAlles() {
+        System.out.println("Zeldal on veel " + zelusid + " elu alles");
     }
 
     public void prindiKasOnElus() {
+        if (zelusid>0){
+            System.out.println("Zelda on elus!");
+        }
+        else
+            System.out.println("Koll võitis selle roundi");
 
     }
 }
